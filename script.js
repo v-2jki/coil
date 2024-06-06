932825e4f34625a71bf80161038296e213f46090
Varun Punia <120302094+v-2jki@users.noreply.github.com>
2024-06-06 09:28:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById('recoil-to-uncoil').addEventListener('click', function() {
+    console.log('Recoil to uncoil button clicked');
+});
+
+document.getElementById('frith-from-weigher').addEventListener('click', function() {
+    console.log('Frith from weigher button clicked');
+});
+
